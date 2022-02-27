aac38cdd7163358520d8a32e3dc0b8041e09bb0e
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2022-02-27 22:12:15 +0100

    fix dates

--- data/experience.ts
+++ data/experience.ts
@@ -3,12 +3,12 @@ export const allPositions = [
     id: 1,
     position: "Blogger",
     company: "zagrodzki.me",
-    date: "02.2021 - present",
+    date: "02.2022 - present",
   },
   {
     id: 2,
     position: "Front-end developer",
     company: "Freelancer",
-    date: "09.2020 - present",
+    date: "09.2021 - present",
   },
 ] as const;
