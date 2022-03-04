9780b3b5036aaa5aae264030c7deee2004c9354e
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2022-03-04 16:34:26 +0100

    fix: minor fixes

--- data/recommendations.ts
+++ data/recommendations.ts
@@ -4,7 +4,7 @@ export const allRecommendations = [
       "Bartek has made tremendous progress over time. I watched his beginnings, and accepting criticism wasn't easy for him. Gradually, Bartek realized the best way to grow as a developer is to learn from other people's experiences. Now he's successfully helping others in the community.",
     author: {
       name: "Michał Miszczyszyn",
-      position: "Lead Engineer in 11Sigma • CEO at Type Of Web",
+      position: "Software Engineer at Briza • CEO at Type Of Web",
       image: "/img/references/michal-miszczyszyn.jpg",
     },
   },
