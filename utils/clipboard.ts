d4bdd7871a4800d2370bef3f3fa9043784163700
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2022-03-11 20:01:19 +0100

    fix: minor fixes

--- utils/clipboard.ts
+++ utils/clipboard.ts
@@ -1,23 +1,16 @@
-export const copyToClipboard = (text: string) =>
-  new Promise((resolve, reject) => {
-    if (navigator?.clipboard) {
-      navigator.clipboard.writeText(text).then(resolve).catch(reject);
-    } else {
-      try {
-        const body = document.querySelector("body");
+export const copyToClipboard = async (text: string) => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+    await navigator.clipboard.writeText(text);
+  } else {
+    const body = document.querySelector("body");
 
-        const textarea = document.createElement("textarea");
-        body?.appendChild(textarea);
+    const textarea = document.createElement("textarea");
+    body?.appendChild(textarea);
 
-        textarea.value = text;
-        textarea.select();
-        document.execCommand("copy");
+    textarea.value = text;
+    textarea.select();
+    document.execCommand("copy");
 
-        body?.removeChild(textarea);
-
-        resolve(text);
-      } catch (e) {
-        reject(e);
-      }
-    }
-  });
+    body?.removeChild(textarea);
+  }
+};
