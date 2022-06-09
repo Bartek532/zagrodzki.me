046a9f2d0a841cfffa3c808f881cecf5c744f373
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2022-06-09 17:09:35 +0200

    publish: Remarkable 2 - what is it and why you should buy it?

--- data/categories.ts
+++ data/categories.ts
@@ -4,6 +4,7 @@ export const allCategories = [
   { slug: "animation", name: "Animation" },
   { slug: "react", name: "React" },
   { slug: "performance", name: "Performance" },
+  { slug: "life", name: "Life" },
   { slug: "js", name: "JavaScript" },
   { slug: "design", name: "Design" },
 ] as const;
