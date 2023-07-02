924cb731f0b68e97616724d74033f611b98b56c0
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-07-02 19:07:41 +0200

    feat: optimize fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,46 @@
+import clsx from "clsx";
+import localFont from "next/font/local";
+
 import { Analytics } from "components/common/analytics/Analytics";
 import { Layout } from "components/layout/Layout";
 import { AppProviders } from "providers/AppProviders";
 
 import "../styles/globals.scss";
+
+const walsheim = localFont({
+  src: [
+    {
+      path: "../public/fonts/GT-Walsheim-Black.woff2",
+      weight: "400",
+    },
+    {
+      path: "../public/fonts/GT-Walsheim-Bold.woff2",
+      weight: "bold",
+    },
+    {
+      path: "../public/fonts/GT-Walsheim-Medium.woff2",
+      weight: "600",
+    },
+    {
+      path: "../public/fonts/GT-Walsheim-Regular.woff2",
+      weight: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-walsheim",
+});
+
+const mono = localFont({
+  src: "../public/fonts/LeagueMono-Regular.woff2",
+  display: "swap",
+  variable: "--font-mono",
+});
+
+const kenfolg = localFont({
+  src: "../public/fonts/Kenfolg.otf",
+  display: "swap",
+  variable: "--font-kenfolg",
+});
 
 export default function RootLayout({
   children,
@@ -15,6 +53,7 @@ export default function RootLayout({
       dir="ltr"
       prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"
       itemType="http://schema.org/WebPage"
+      className={clsx(walsheim.variable, mono.variable, kenfolg.variable)}
     >
       <body>
         <AppProviders>
