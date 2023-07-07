dca331d6fcb20be98004020ea7787ca71510a906
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-07-07 10:36:58 +0200

    fix: include 404 page in app dir

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,21 @@
 import { About } from "components/about/about";
 import { Hero } from "components/common/hero/Hero";
 import { getMetadata } from "lib/metadata";
-import { getAllResourcesTotalViews } from "lib/views";
 
-const description = "Want to know more about me? You've come to the right place 🎓";
+const description =
+  "Want to know more about me? You've come to the right place 🎓";
 
-export const metadata = getMetadata({ title: "About", description, image: "/img/about.png" });
+export const metadata = getMetadata({
+  title: "About",
+  description,
+  image: "/img/about.png",
+});
 
-const AboutPage = async () => {
-  const views = await getAllResourcesTotalViews();
-
-  return (
-    <>
-      <Hero title="About" description={description} />
-      <About views={views} />
-    </>
-  );
-};
+const AboutPage = () => (
+  <>
+    <Hero title="About" description={description} />
+    <About />
+  </>
+);
 
 export default AboutPage;
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import { Custom404 } from "components/404/404";
+
+const NotFoundPage = () => <Custom404 />;
+
+export default NotFoundPage;
