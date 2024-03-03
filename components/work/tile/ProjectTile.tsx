bf816a41649318d06ff6c03da2a76133c24f083f
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2024-03-03 12:57:55 +0100

    fix: mockups cleanup

--- components/work/tile/ProjectTile.tsx
+++ components/work/tile/ProjectTile.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { memo } from "react";
 
 import { Image } from "components/common/image/Image";
+import { useTheme } from "providers/ThemeProvider";
 import Arrow from "public/svg/right-top-arrow.svg";
 
 import styles from "./projectTile.module.scss";
@@ -18,6 +19,7 @@ interface ProjectTileProps {
 }
 
 export const ProjectTile = memo<ProjectTileProps>(({ project, mockupPosition }) => {
+  const { theme } = useTheme();
   const imageVariants = {
     hover: {
       rotate: mockupPosition === "right" ? "-23.5deg" : "23.5deg",
@@ -30,7 +32,7 @@ export const ProjectTile = memo<ProjectTileProps>(({ project, mockupPosition }) 
         whileHover="hover"
         className={styles.project}
         style={{
-          backgroundImage: `url(/img/projects/${project.slug}/back.png)`,
+          backgroundImage: `url(/img/projects/${project.slug}/mockup/back.png)`,
         }}
       >
         <span className="sr-only">check my project - {project.title}</span>
@@ -42,7 +44,12 @@ export const ProjectTile = memo<ProjectTileProps>(({ project, mockupPosition }) 
             y: "-45%",
           }}
         >
-          <Image src={`/img/projects/${project.slug}/mockup.png`} alt="" height="592" width="330" />
+          <Image
+            src={`/img/projects/${project.slug}/mockup/${theme}.png`}
+            alt=""
+            height="592"
+            width="330"
+          />
         </motion.div>
         <div className={styles.arrow}>
           <Arrow />
