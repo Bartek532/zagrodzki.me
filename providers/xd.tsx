c00b4e0f3fcd72e9cdb57a505e78443c5e664c44
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2024-03-08 19:58:21 +0100

    fix: theme change

--- providers/xd.tsx
+++ providers/xd.tsx
@@ -7,52 +7,43 @@ import { useTheme } from "./ThemeProvider";
 export const ThemeChanger = ({ children }: { children: React.ReactNode }) => {
   const { theme } = useTheme();
 
-  const [fgTheme, bgTheme] = theme === "light" ? ["dark,light"] : ["light", "dark"];
+  const [fgTheme, bgTheme] = theme === "light" ? ["dark", "light"] : ["light", "dark"];
 
   return (
     <AnimatePresence>
-      <span
+      <motion.div
         data-theme={fgTheme}
-        key={`span-theme-${fgTheme}`}
+        key={`theme-${fgTheme}`}
+        initial={{
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+        }}
+        animate={{ clipPath: "polygon(0 0, 0 0, 0 100%, 0 100%)" }}
+        transition={{ duration: 1.5, ease: [0.17, 0.5, 0.9, 1] }}
         style={{
-          position: "relative",
+          backgroundColor: "var(--bg-100)",
+          color: "var(--black-100)",
+          position: "absolute",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 10,
         }}
       >
-        <motion.div
-          key={`theme-${fgTheme}`}
-          initial={{
-            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-          }}
-          animate={{ clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)" }}
-          transition={{ duration: 3, ease: [0.17, 0.5, 0.9, 1] }}
-          style={{
-            backgroundColor: "var(--bg-100)",
-            color: "var(--black-100)",
-            position: "absolute",
-            width: "100vw",
-            height: "100vh",
-            zIndex: 10,
-          }}
-        >
-          {children}
-        </motion.div>
-      </span>
-
-      <span key={`span-theme-${theme}`} data-theme={bgTheme}>
-        <motion.div
-          key={`theme-${bgTheme}`}
-          style={{
-            backgroundColor: "var(--bg-100)",
-            color: "var(--black-100)",
-            position: "absolute",
-            width: "100vw",
-            height: "100vh",
-            zIndex: 1,
-          }}
-        >
-          {children}
-        </motion.div>
-      </span>
+        {children}
+      </motion.div>
+      <motion.div
+        data-theme={bgTheme}
+        key={`theme-${fgTheme}`}
+        style={{
+          backgroundColor: "var(--bg-100)",
+          color: "var(--black-100)",
+          position: "absolute",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 1,
+        }}
+      >
+        {children}
+      </motion.div>
     </AnimatePresence>
   );
 };
